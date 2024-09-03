18389c0165b7e03ea401b9e0da8ce873c02bd3b3
r41l1m4 <rai.lima17@gmail.com>
2024-09-03 09:35:20 -0300

    add account creation and overview message

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -16,16 +16,18 @@ public class ContaTerminal {
         numeroConta = new Scanner(System.in).nextInt();
 
         System.out.println("Que tal se você nos disser seu nome completo?");
-        nomeCliente = new Scanner(System.in).next();
+        nomeCliente = new Scanner(System.in).nextLine();
 
         System.out.println("Só um instante enquanto criamos sua conta...");
 
-
+        Conta novaConta = Conta.novaConta(numeroConta, agencia, nomeCliente);
         Thread.sleep(2000);
 
         System.out.println("Prontinho!");
-
-
+        System.out.println("Olá, " + novaConta.getNomeCliente() + "!. " +
+                "Obrigado por criar uma conta em nosso banco. Sua agência é " +
+                novaConta.getAgencia() + ", conta " + novaConta.getNumero() +
+                " e seu saldo por enquanto é R$ " + novaConta.getSaldo());
 
     }
 }
